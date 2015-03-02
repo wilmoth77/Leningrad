e5f6e1609bec934d2b66598d8a6652f1ee19b114
wilmoth77 <jeff@wilmoth77.com>
2015-03-02 11:58:01 -0500

    Wrapper changes

---
+++ root/base.php
@@ -0,0 +1,25 @@
+<?php get_template_part('templates/head'); ?>
+  <body <?php body_class(); ?>>
+    <?php
+      do_action('get_header');
+      get_template_part('templates/header');
+    ?><!-- Base.php -->
+
+    <div class="wrap container" role="document">
+      <div class="content row">
+        <main class="main" role="main">
+          <?php include {%= prefix %}_template_path(); ?>
+        </main><!-- /.main -->
+        <?php if ({%= prefix %}_display_sidebar()) : ?>
+          <aside class="sidebar" role="complementary">
+            <?php include {%= prefix %}_sidebar_path(); ?>
+          </aside><!-- /.sidebar -->
+        <?php endif; ?>
+      </div><!-- /.content -->
+    </div><!-- /.wrap -->
+    <?php
+      get_template_part('templates/footer');
+      wp_footer();
+    ?>
+  </body>
+</html>
--- root/inc/config.php
+++ root/inc/config.php
@@ -16,3 +16,56 @@ add_theme_support('soil-nice-search');      // Enable /?s= to /search/ redirect 
 add_theme_support('soil-disable-trackbacks');   // Remove trackback/pingback functionality
 add_theme_support('bootstrap-gallery');     // Enable Bootstrap's thumbnails component on [gallery]
 add_theme_support('jquery-cdn');            // Enable to load jQuery from the Google CDN
+
+/**
+ * Add body class if sidebar is active
+ */
+function {%= prefix %}_sidebar_body_class($classes) {
+  if ({%= prefix %}_display_sidebar()) {
+    $classes[] = 'sidebar-primary';
+  }
+  return $classes;
+}
+add_filter('body_class', '{%= prefix %}_sidebar_body_class');
+
+/**
+ * Define which pages shouldn't have the sidebar
+ *
+ * See inc/sidebar.php for more details
+ */
+function {%= prefix %}_display_sidebar() {
+  $sidebar_config = new {%= prefix %}_Sidebar(
+    /**
+     * Conditional tag checks (http://codex.wordpress.org/Conditional_Tags)
+     * Any of these conditional tags that return true won't show the sidebar
+     *
+     * To use a function that accepts arguments, use the following format:
+     *
+     * array('function_name', array('arg1', 'arg2'))
+     *
+     * The second element must be an array even if there's only 1 argument.
+     */
+    array(
+      'is_404',
+      'is_front_page'
+    ),
+    /**
+     * Page template checks (via is_page_template())
+     * Any of these page templates that return true won't show the sidebar
+     */
+    array(
+      'template-custom.php'
+    )
+  );
+
+  return apply_filters('{%= prefix %}/display_sidebar', $sidebar_config->display);
+}
+
+/**
+ * $content_width is a global variable used by WordPress for max image upload sizes
+ * and media embeds (in pixels).
+ *
+ * Example: If the content area is 640px wide, set $content_width = 620; so images and videos will not overflow.
+ * Default: 1140px is the default Bootstrap container width.
+ */
+if (!isset($content_width)) { $content_width = 1140; }
